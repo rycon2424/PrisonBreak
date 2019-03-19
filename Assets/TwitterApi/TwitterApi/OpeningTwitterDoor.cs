8facb02849f377e7e458e82552a7d88bd7925d8f
Bosko Ivkovic <Moordkiller1>
2019-03-19 10:05:33 +0100

    Adding the Twitter API and json

---
+++ Assets/TwitterApi/TwitterApi/OpeningTwitterDoor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningTwitterDoor : MonoBehaviour
+{
+
+    public GameObject twitterGateMenu;
+    public static bool opening;
+
+    public Animator doorAnim;
+
+    void Start()
+    {
+        twitterGateMenu.SetActive(false);
+        opening = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Hide();
+        }
+
+        if (opening == true)
+        {
+            doorAnim.Play("Open2");
+            Hide();
+        }
+    }
+
+    void Hide()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        twitterGateMenu.SetActive(false);
+    }
+
+    public void ShowTwitterMenu()
+    {
+        twitterGateMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
